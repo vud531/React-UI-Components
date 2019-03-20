9ba9def68a87976a35314662b82decf37fe4c253
Duc Vu <de.yd315@gmail.com>
2019-03-20 05:29:59 -0500

    added header container

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+export default () => {
+    return (
+        <div>
+            <ImageThumbnail/>
+            <HeaderTitle/>
+            <HeaderContent/>
+        </div>
+    )
+}
+    
