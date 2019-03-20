e22a7687e140725637ad18703b93c8d00211c2b2
Duc Vu <de.yd315@gmail.com>
2019-03-19 21:14:55 -0500

    fixed compiler erros and warnings

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
+import NumberButton from "./components/ButtonComponents/NumberButton";
+const numbers = [0,1,2,3,4,5,6,7,8,9];
+const numberBtns = [];
+numbers.forEach(number => numberBtns.push({ "content": number }));
+console.log(numberBtns);
+
+const actions = ["+", "-", "x", "/", "=", "clear"];
+const actionBtns = [];
+actions.forEach(action => actionBtns.push({ "content": action }));
 
 const App = () => {
+
   return (
     <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <div>
+        <p className="display"></p>
+      </div>
+      <div className="numbersPanel">
+        {numberBtns.map(num => (
+          <NumberButton key={num.content} numberButtonProp={num} />
+        ))}
+      </div>
     </div>
   );
 };
